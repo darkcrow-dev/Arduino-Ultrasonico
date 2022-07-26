42c964e32bbf699dfdb9b1610e2a67e1b19404bc
Darkcrow93 <108247794+Darkcrow2@users.noreply.github.com>
2022-07-26 02:44:24 -0500

    Add files via upload

---
+++ Ultrasonico.ino
@@ -0,0 +1,39 @@
+// DECLARACION DE VARIABLES PARA PINES
+const int pinecho = 8; //Entrada
+const int pintrigger = 9; //Salida
+ 
+// VARIABLES PARA CALCULOS
+unsigned int tiempo, distancia;
+ 
+void setup() {
+  // PREPARAR LA COMUNICACION SERIAL
+  Serial.begin(9600);
+  
+  // CONFIGURAR PINES DE ENTRADA Y SALIDA
+  pinMode(pinecho, INPUT);
+  pinMode(pintrigger, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+ 
+void loop() {
+  // ENVIAR PULSO DE DISPARO EN EL PIN "TRIGGER"
+  digitalWrite(pintrigger, LOW);
+  delayMicroseconds(4);
+  digitalWrite(pintrigger, HIGH);
+  
+  // EL PULSO DURA AL MENOS 10 uS EN ESTADO ALTO
+  delayMicroseconds(10);
+  digitalWrite(pintrigger, LOW);
+ 
+  // MEDIR EL TIEMPO EN ESTADO ALTO DEL PIN "ECHO" EL PULSO ES PROPORCIONAL A LA DISTANCIA MEDIDA
+  tiempo = pulseIn(pinecho, HIGH);
+ 
+  // LA VELOCIDAD DEL SONIDO ES DE 340 M/S O 29 MICROSEGUNDOS POR CENTIMETRO
+  // DIVIDIMOS EL TIEMPO DEL PULSO ENTRE 58, TIEMPO QUE TARDA RECORRER IDA Y VUELTA UN CENTIMETRO LA ONDA SONORA
+  distancia = tiempo / 58;
+ 
+  // ENVIAR EL RESULTADO AL MONITOR SERIAL
+  Serial.print(distancia);
+  Serial.println(" cm");
+  delay(200);
+}
